d0d4f103c0d6d974321b3630ab3d75f79fbc0553
momin morgan <mominmorgan10@gmail.com>
2023-08-31 14:58:50 -0700

    hot fix betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-
 /**
  * binary_tree_insert_right - insert tight child node
  * @parent:pointer to the right child node to be inserted
  * @value: integer to be stored
- * Return: pointer to new node 
+ * Return: pointer to new node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
