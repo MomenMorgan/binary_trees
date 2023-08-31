fbf03135c27892bc70c276651c300715823e06a4
momin morgan <mominmorgan10@gmail.com>
2023-08-31 15:41:17 -0700

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function that measures the depth 
+ *of a node in a binary tree
+ * @tree: node to mesures
+ * Return: levels from root
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (!tree)
+		return (0);
+
+	for (depth = 0; tree->parent; depth++)
+		tree = tree->parent;
+
+	return (depth);
+}
