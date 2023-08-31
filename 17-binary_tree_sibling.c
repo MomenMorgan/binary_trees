de89482ff6f46dae1003309fa7c18fd4c0963d80
momin morgan <mominmorgan10@gmail.com>
2023-08-31 16:50:27 -0700

    sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling -  finds the sibling of a node
+ * @node: node to check
+ * Return: pointer to node sharing same parent 
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	if (node->parent)
+	{
+		if (node->parent->left == node)
+			return (node->parent->right);
+		if (node->parent->right == node)
+			return (node->parent->left);
+	}
+	return (NULL);
+}
