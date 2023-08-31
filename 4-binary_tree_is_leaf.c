0e6b25535fedbb1d8cf6aef410c467866cb65615
momin morgan <mominmorgan10@gmail.com>
2023-08-31 15:05:45 -0700

    leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checkes if binary tree's node is a leaf
+ * @node: node to be checked
+ * Return: 1 for true 0 for false
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left || node->right)
+		return (0);
+	else
+		return (1);
+}
