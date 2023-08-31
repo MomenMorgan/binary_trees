447c4128a97c18c93c58667f5a2d7353181c9a00
momin morgan <mominmorgan10@gmail.com>
2023-08-31 07:30:42 -0700

    fixing print

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,6 +19,6 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+void binary_tree_print(const binary_tree_t *);
 
 #endif
